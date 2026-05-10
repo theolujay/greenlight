646f6255947e942b73d9d60063126c4646d3bae3
Joseph Ezekiel <theolujay@gmail.com>
2026-05-10 09:18:42 +0100

    feat: add rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
+
+	"github.com/tomasen/realip"
+	"golang.org/x/time/rate"
 )
 
 // The recoverPanic() method is a middleware for the server to send a
@@ -35,6 +40,85 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Hold the rate limiter and last seen time for each client.
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	// A mutex and a map to hold the clients' IP addresses
+	// and rate limiters.
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Launch a background goroutine which removes old entries from
+	// the clients map once every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// Lock the mutex to prevent any rate limiter checks from
+			// happening while the cleanup is taking place.
+			mu.Lock()
+
+			// Loop through all clients. If they haven't been seen
+			// within the last three minutes, delete the corresponding
+			// entry from the map.
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			// Unlock the mutext when the clean up is complete
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.config.limiter.enabled {
+			// Get client's IP address
+			ip := realip.FromRequest(r)
+
+			mu.Lock()
+
+			// Check to see if the IP address already exists in the map. If
+			// it doesn't , then initialize the new rate limiter and add the
+			// IP address and limier to the map.
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(
+						rate.Limit(app.config.limiter.rps),
+						app.config.limiter.burst,
+					),
+				}
+			}
+
+			// Update the last seen time for the client
+			clients[ip].lastSeen = time.Now()
+
+			// Call the Allow() method on the rate limiter for the current
+			// IP address. If the request isn't allowed, unlock the mutex
+			// and send a 429 Too Many Requests response.
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
+			// Make sure to unlock the mutex before calling the next handler
+			// in the chain. And notice how `defer` isn't used to unlock the
+			// mutext, as that would mean that the mutext isn't unlocked
+			// until all the handlers downstream of this middleware have
+			// also returned.
+			mu.Unlock()
+		}
 
 		next.ServeHTTP(w, r)
 	})
--- go.mod
+++ go.mod
@@ -7,4 +7,8 @@ require (
 	github.com/justinas/alice v1.2.0
 )
 
-require github.com/lib/pq v1.12.3
+require (
+	github.com/lib/pq v1.12.3
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
+	golang.org/x/time v0.15.0
+)
